3b15b7c21510b33b2a29f2dbcf81d86091c77e8e
Andrew <mylifescrazy.phifer19@gmail.com>
2025-04-11 16:49:11 -0500

    modified markdown page loading routines to be a bit more reusable.  really need to learn much more about javascript promises and closures.  now has a function to load hero content, which automatically sets the page title based off of the markdown page's title meta data field.

--- assets/javascript/load-markdown-document.js
+++ assets/javascript/load-markdown-document.js
@@ -4,16 +4,37 @@ var getMarkDownPage = (pageName, target) => {
     fetch(markDownPageRequest)
         .then((response) => response.text())
         .then((text) => {
-            var pageContent = text;
-            var converter = new showdown.Converter({
-                metadata: true
-            });
-            converter.setFlavor('github')
-            var htmlData = converter.makeHtml(pageContent);
-            document.getElementById(target).innerHTML = htmlData;
-            console.log(htmlData)
-            console.log(pageContent)
-        });        
+            markDownDecode(text, target)
+    });
+
+    
+}
+
+
+var markDownDecode = (text, targetID) => {
+    var pageContent = text;
+    var converter = new showdown.Converter({
+        metadata: true
+    });
+    converter.setFlavor('github')
+    var htmlData = converter.makeHtml(pageContent);
+    document.getElementById(targetID).innerHTML = htmlData;
+    var metaData = converter.getMetadata()
+    console.log(htmlData)
+    console.log(pageContent)
+    return metaData
+}
+
+
+var getHeroContent = (pageName, target) => {
+    var markDownPageRequest = new Request(pageName)    
+
+    fetch(markDownPageRequest)
+        .then((response) => response.text())
+        .then((text) => {
+            metaData = markDownDecode(text, target)
+            document.title = metaData['title']
+    });
 }
 
 
